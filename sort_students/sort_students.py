b59f10ba6a95badefae3bbb2fd90d689034a586b
shawn sudaria <46009834+Sh-DFIR@users.noreply.github.com>
2022-04-29 21:50:00 +0800

    Update

--- sort_students/sort_students.py
+++ sort_students/sort_students.py
@@ -1,18 +1,14 @@
-# MY CODE
+## MY CODE ##
 def sort_a_list(a_list):
-    return sorted(a_list)
+    return sorted(a_list, reverse=True)
 
 
 def sort_by_mark(my_class):
-    # dic = {}
     li = []
     for tup in my_class:
         li.append(tup)       
     
-    # tuple = sorted(dic.items(), reverse=True) 
-    # for i in tuple:
-    #     print(i)
-    return sorted(li, reverse=True)
+    return sorted(li, reverse=True) 
 
 def test(li):
     return li[1]
@@ -24,16 +20,20 @@ def sort_by_name(my_class):
     return sorted(li, key=test, reverse=False)
 
 
+
 # Uncomment the following lines as needed
 # if you want to test your implementation a bit:
 
 # print(sort_a_list([3, 2, 1]))  # should print [1, 2, 3]
+
 # my_class = [(25, "Shannon"), (50, "Alan"), (75, "Ada")]
-# # print(sort_by_mark(my_class))  #should print []
+
+# print(sort_by_mark(my_class))
 # print(sort_by_name(my_class))
 
 
-# SOMEONE ELSES
+
+## SOMEONE ELSES ##
 #!/usr/bin/env python3
 
 def sort_a_list(a_list):
