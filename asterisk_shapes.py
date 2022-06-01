212e1632ea41c4ab0406294a55376aa6a1311cd8
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-06-01 14:13:28 +0800

    New File

---
+++ asterisk_shapes.py
@@ -0,0 +1,12 @@
+n = 0
+for i in range(5):
+    n += 1
+    print("*" * n, end="  ")
+    print("")
+
+# rows = 5
+# for i in range(rows + 1):
+#     for j in range(i):
+#         print(i, end='')
+
+#      print('')
