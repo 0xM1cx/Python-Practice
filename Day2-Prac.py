a9692a9e9331664e4b39683923c5b8dcf291b6a9
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-07-30 09:53:34 +0800

    Day 2 Python Practice

---
+++ Day2-Prac.py
@@ -0,0 +1,8 @@
+def questionFive():
+    '''Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.'''
+    firstName = input("FirstName: ")
+    lastName = input("LastName: ")
+    print(f"{LastName} {FirstName}")
+
+
+questionFive()
