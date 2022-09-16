364941ef1f2f21bdc082a7cbe092d160899ef5da
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-09-16 10:56:22 +0800

    Updated

--- higherlower.py
+++ higherlower.py
@@ -3,25 +3,25 @@ import random
 def higherLower():
     '''Create a program where the player will guess the exact positive integer the bot is holding'''
     botNumber = random.randint(0, 1000)
-    nTurns = 0
+    nTurns = 4
     answerStatus = True
     try:
-        while answerStatus:
-            playerInput = int(input("Your Guess: "))
+        while answerStatus and nTurns:
+            playerInput = int(input("Bot: Guess the correct answer...\nYour answer: "))
             if playerInput > botNumber:
-                print("Lower")
-                nTurns += 1
+                print("Bot: Lower!\n")
+                nTurns -= 1
             elif playerInput < botNumber:
-                nTurns += 1
-                print("Higher")
+                print("Bot: Higher!\n")
+                nTurns -= 1
             else:
                 print(f"Correct! You guessed the correct answer in {nTurns} turns")
                 answerStatus = False
+
+        print("Bot: No more turns left")
     except:
         print("Input must be an Integer")
 
+
 higherLower()
 
-    
-        
-    
