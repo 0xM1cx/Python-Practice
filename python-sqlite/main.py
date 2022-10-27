7376243c995329edd68cad10d66c0007c689e078
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-10-27 11:09:15 +0800

    SQLite Practice Files

---
+++ python-sqlite/main.py
@@ -0,0 +1,11 @@
+from multiprocessing import connection
+import sqlite3
+
+connection = sqlite3.connect("students.db")
+cursor = sqlite3.cursor()
+
+cursor.execute("create table students (Name text, School text, Age integer)")
+
+students = [("Shawn Michael A. Sudaria", "EVSU", 18), ("Chelsea P. Zarzuela", "EVSU", 18)]
+
+connection.close()
