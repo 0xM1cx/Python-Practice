ff26046fce3769b60c861f9b37675caef47b5053
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-10-27 11:15:16 +0800

    Updated

--- python-sqlite/main.py
+++ python-sqlite/main.py
@@ -8,4 +8,6 @@ cursor.execute("create table students (Name text, School text, Age integer)")
 
 students = [("Shawn Michael A. Sudaria", "EVSU", 18), ("Chelsea P. Zarzuela", "EVSU", 18)]
 
+cursor.execute(many)
+
 connection.close()
