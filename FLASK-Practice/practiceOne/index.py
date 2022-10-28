6d3430c8ec4ac0096f2d5dcde00fe55e20ecad01
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-10-28 11:22:36 +0800

    updated

--- FLASK-Practice/practiceOne/index.py
+++ FLASK-Practice/practiceOne/index.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, escape, url_for
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
-    return "Welcome!!!"
+    return "Welcome!!! User"
+
+@app.route('/about')
+def about():
+    return "<h1>About Me</h1>"
+
+@app.route('/user/<name>')
+def user(name):
+    return f"Welcome {escape(name)}"
+
+with app.test_request_context():
+    print(url_for('user', name="Shawn"))
+    print(url_for('about'))
+    print(url_for('index'))
