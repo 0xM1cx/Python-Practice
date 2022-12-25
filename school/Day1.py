f4a01e542f5e422e56c2354e1905ed5de3837ccb
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-25 15:59:43 +0800

    Python review | multiple arguments & recursive calls

---
+++ school/Day1.py
@@ -0,0 +1,15 @@
+# Day 1 of python review
+
+# Arbitrary number of arguments
+
+def main(*ages):
+    for i in ages:
+        print(i)
+
+    print("----------------")
+
+    for b in range(0, len(ages)):
+        print(ages[b])
+
+
+main(1, 2, 3, 4, 5, 6, 7, 7, 8)
