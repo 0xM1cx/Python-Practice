9d1ee69bfb2e4fecf34353e632c137025b512954
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-26 19:55:29 +0800

    Learning DSA

--- school/Day1.py
+++ school/Day1.py
@@ -1,5 +1,5 @@
 # Day 1 of python review
-
+from math import sqrt
 ## Arbitrary number of arguments
 
 def main(*ages):
@@ -16,6 +16,38 @@ def main(*ages):
 
 
 ## Lambda Function Practice
-birthYear = int(input())
-user_age = lambda birthYear : 2022 - birthYear
-print(f"You are now {user_age(birthYear)} years old")
+# birthYear = int(input())
+# user_age = lambda birthYear : 2022 - birthYear
+# print(f"You are now {user_age(birthYear)} years old")
+
+# name = "Shawn"
+
+
+# def testOUt():
+#     f = user_age(birthYear)
+    
+#     global name
+
+#     print(name)
+    
+#     name = "Crystal"
+#     print(name)
+
+
+
+
+raw_data = input("INPUT YOUR DATA: ").split(" ")
+converted_data = [int(x) for x in raw_data]
+x_bar = 0
+def getStandardDeviation(data):
+    global x_bar
+    x_bar = sum(data) / len(data)
+    result = 0
+    for i in data:
+        result += ((i - x_bar)**2)
+    s_dev = result / (len(data) - 1)
+
+    return round(sqrt(s_dev), 2)
+
+print(f"The stadard Deviation of the given data is {getStandardDeviation(converted_data)}")
+print(f"The average of the data is {x_bar}")
