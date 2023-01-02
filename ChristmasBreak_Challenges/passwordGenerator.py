6cdbed122a63497e760657dd718d1980a65e05d5
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-03 00:25:43 +0800

    Started password generator

--- ChristmasBreak_Challenges/passwordGenerator.py
+++ ChristmasBreak_Challenges/passwordGenerator.py
@@ -1,4 +1,17 @@
 import random
+import string
 
 passSize = int(input("What is the length of the password? "))
+print("What combinations will you password have?")
+print("If you want uppercase letters type U, lowercase letters type l, digits with d and special characters with s")
+print("Example input: s")
 
+
+lowercase_letters = string.ascii_lowercase
+uppercase_letters = string.ascii_uppercase
+digits = string.digits
+special_characters = "!@#$%^&*"
+
+password = ""
+for i in range(passSize):
+    password.append()
