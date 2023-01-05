b4adea46c767b3f914a1acd3136bb47ca5c1eec5
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-06 07:12:42 +0800

    Multiple Errors

--- FLASK-Practice/practiceTwo/main.py
+++ FLASK-Practice/practiceTwo/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, url_for, render_template, escape
+from flask import Flask, url_for, render_template, escape, request
 import sqlite3
 from datetime import datetime
 from time import sleep
+import requests
 
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
     
-
+currentindex = 0
 @app.route('/rate')
 def voting():
 
@@ -21,7 +22,13 @@ def voting():
     profiles = conn.execute("SELECT * FROM instructors").fetchall()
     conn.close()
     
-    return render_template("rate.html", profiles=profiles)
+    global currentindex
+    currentindex = currentindex + 1
+    # next_item = request.form.get('next_item')
+    # next_item_index = int(request.form.get('next_item_index', 0))
+    # data = conn.query(column='Nauka').offset(next_item_index).limit(1).all()[0]
+    # data = conn.execute("SELCT * FROM instructors WHERE College LIKE '%COE%'")
+    return render_template("rate.html", profiles=profiles, currentindex=currentindex)
 
 
 
