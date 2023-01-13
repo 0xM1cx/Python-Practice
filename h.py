fc4dfb2f305665589137cc377e223135a7a97855
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-13 18:16:42 +0800

    Updated Matplotlib practice file

--- h.py
+++ h.py
@@ -1,42 +1,42 @@
-# import matplotlib.pyplot as akonPlotter
+import matplotlib.pyplot as akonPlotter
 
+for i in range(1,5):
+    akonData = {
+        "Kagwapohan pag nasa EVSU": i,
+        "Kagwapohan pag nasa Boarding house": 50,
+        "Charm": 70,
+        "Mapagmahal rate": 70,
+        "Loyal rate": 100
+    }
 
-# akonData = {
-#     "Math": 40,
-#     "Physics": 50,
-#     "Chemistry": 40,
-#     "Computer Science": 70,
-#     "Philosophy": 50
-# }
+    subjects = list(akonData.keys())
+    numberOfEnrollees = list(akonData.values())
 
-# subjects = list(akonData.keys())
-# numberOfEnrollees = list(akonData.values())
+    figure = akonPlotter.figure(figsize=(40,5))
 
-# figure = akonPlotter.figure(figsize=(10,5))
+    akonPlotter.bar(subjects, numberOfEnrollees, color = "blue", width=0.5)
 
-# akonPlotter.bar(subjects, numberOfEnrollees, color = "blue", width=0.5)
+    akonPlotter.xlabel("Criteria para Kay Shawn")
+    akonPlotter.ylabel("Rating")
+    akonPlotter.title("Rating ng Mga EVSU Student ni Shawn")
+    akonPlotter.show()
 
-# akonPlotter.xlabel("Mga subjects")
-# akonPlotter.ylabel("Mga Number of Enrollees")
-# akonPlotter.title("Number of students enrolled in sample courses")
-# akonPlotter.show()
+# def digit_sum(n):
+#     n = str(n)
+#     sum = 0
+#     for i in n:
+#         sum += int(i)
+#     return sum 
 
-def digit_sum(n):
-    n = str(n)
-    sum = 0
-    for i in n:
-        sum += int(i)
-    return sum 
-
-def longest_word(text):
-    text = text.replace("\n", " ")
-    words = text.split(" ")
-    longestword = " "
-    for i in words:
-        if len(i) >= len(longestword):
-            print(i)
+# def longest_word(text):
+#     text = text.replace("\n", " ")
+#     words = text.split(" ")
+#     longestword = " "
+#     for i in words:
+#         if len(i) >= len(longestword):
+#             print(i)
             
             
-    print()
+#     print()
 
-longest_word("The king is the current\nking")
+# longest_word("The king is the current\nking")
