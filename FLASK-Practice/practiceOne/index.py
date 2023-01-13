9811d7f02ec3ec9252e003730ea1e5fe9e96720d
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-14 02:03:11 +0800

    Updated

--- FLASK-Practice/practiceOne/index.py
+++ FLASK-Practice/practiceOne/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, url_for, render_template
+from flask import Flask, escape, url_for, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,4 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/cipher")
+@app.route("/<name>")
+def sample(name=None):
+    return render_template("CaesarCipher.html", username=name)
 
