abfcd31f0b708fead7a7dc6fccc799c90777b34c
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-14 18:02:03 +0800

    Update

--- HackInScience/Python_Training/checkPrime.py
+++ HackInScience/Python_Training/checkPrime.py
@@ -1,12 +1,19 @@
 def is_prime(n):
-
+    flag = 0;
     if n == 1 or n == 0 or n == 2:
-        return False
+        return 1
     elif n > 2:
         for i in range(2, n):
             if n % i == 0:
-                return False
+                flag = 1
             else: 
-                return True
+                flag = 0
 
+
+    if flag == 0:
+        return True
+    else:
+        return False
+
+        
 print(is_prime(9))
