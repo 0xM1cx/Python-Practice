1e746456d5b601566e08c00f1442be4e3dd08d04
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-14 16:14:13 +0800

    Solved Distance Problem

--- HackInScience/Python_Training/distance.py
+++ HackInScience/Python_Training/distance.py
@@ -1,11 +1,12 @@
-def getDistance(arr):
+def dist(arr):
     greatestVal = max(arr)
     lowestVal = min(arr)
-
-    if greatestVal < 0 or lowestVal < 0:
-        return lowestVal + greatestVal;
+   
+    if greatestVal < 0 and lowestVal < 0:
+        return greatestVal - lowestVal
+    elif greatestVal < 0 or lowestVal < 0:
+        return abs(lowestVal) + abs(greatestVal);
     else:
         return greatestVal - lowestVal
 
-
-print(getDistance([1, 2, 3]))
+print(dist([-1, -2, -3]))
