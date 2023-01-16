66e642918b546b31b5a9e86687669ccbd270b01c
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-17 00:32:01 +0800

    Learned Stack | Learning Queue

---
+++ DSA/QueuePractice.py
@@ -0,0 +1,31 @@
+class Queue:
+    def __init__(self):
+        self.queue =  []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop(0)
+    
+    def display(self):
+        print(self.queue)
+
+
+q = Queue()
+
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+q.display()
+print(q.dequeue())
+q.display()
+print(q.dequeue())
+q.display()
+q.enqueue(15)
+q.display()
---
+++ DSA/stackPractice.py
@@ -0,0 +1,25 @@
+def create_stack():
+    stack = []
+    return stack
+
+
+def push(stack, item):
+    stack.append(item)
+
+def pop(stack):
+    if len(stack) > 0:
+        return stack.pop()
+
+
+
+
+s = create_stack()
+
+push(s, 1)
+push(s, 2)
+push(s, 3)
+push(s, 4)
+push(s, 5)
+push(s, 6)
+
+print(s)
