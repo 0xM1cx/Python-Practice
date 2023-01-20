97cde91b611567f1cea2020986c32ff7e8329feb
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-20 15:37:47 +0800

    CodeChef Challenge | Excluding penish.py

---
+++ CodeChef/BiryaniClasses.py
@@ -0,0 +1,4 @@
+testCases = input()
+for i in range(int(testCases)):
+    uInput = input().split(' ')
+    print(int(uInput[0]) * int(uInput[1]))
--- power.py
+++ power.py
@@ -35,5 +35,7 @@ def lambdaPractice():
     dict_list = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
     sorted_dictList = sorted(dict_list, key=lambda a: int(a['model']))
     print(dict_list)
+
+    
     
 lambdaPractice()
