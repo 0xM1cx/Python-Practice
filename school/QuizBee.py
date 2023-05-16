c32cc50561bb30effe0a8e534f390c370738e043
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-05-17 02:28:06 +0800

    update

---
+++ school/QuizBee.py
@@ -0,0 +1,15 @@
+def questionOne():
+    '''OUTPUT
+    0
+    2
+    4
+    6
+    8'''
+    for i in range(10):
+        if i % 2 == 0:
+            print(i)
+
+def questionTwo():
+    
+
+questionTwo()
