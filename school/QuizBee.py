b03cc500aa672961c8db74ee817a707d7bc4c33a
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-24 17:31:42 +0800

    Update

--- school/QuizBee.py
+++ school/QuizBee.py
@@ -1,30 +1,34 @@
-def questionOne():
-    '''OUTPUT
-    0
-    2
-    4
-    6
-    8'''
-    for i in range(10):
-        if i % 2 == 0:
-            print(i)
+# def questionOne():
+#     '''OUTPUT
+#     0
+#     2
+#     4
+#     6
+#     8'''
+#     for i in range(10):
+#         if i % 2 == 0:
+#             print(i)
 
 
-def getThis():
-	return "INTEL"
+# def getThis():
+# 	return "INTEL"
 
 
-def main():
-	uInput = getThis()
-	return uInput + "!!!"
+# def main():
+# 	uInput = getThis()
+# 	return uInput + "!!!"
 
 
-def start():
-	print("b", end='')
-	def end():
-		return "a"
-	print(end(), end='')
-	print("g")
+# def start():
+# 	print("b", end='')
+# 	def end():
+# 		return "a"
+# 	print(end(), end='')
+# 	print("g")
 
-def example():
-	print("\\")
+# def example():
+# 	print("\\")
+
+list_name = ["Shawn", "Jason", "Justing", "Angel"]
+
+print(list_name[0][0])
